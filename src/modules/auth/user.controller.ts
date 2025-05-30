af6bd8c6068f50ce09596b6f1c17935ea41ae313
Prakash Pandey <prakash.pandey@your-space.in>
2025-05-30 16:11:38 +0530

    Many changes

---
+++ src/modules/auth/user.controller.ts
@@ -0,0 +1,56 @@
+import { NextFunction, Request, Response } from "express";
+import { prisma } from "../../config/db/dbconfig";
+import { updateUserSchema } from "../../schema/user.schema";
+import { createError, createSuccess } from "../../utils/messageResponse";
+
+export const updateUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { email, name, departments, roles, user_type, isActive } =
+      updateUserSchema.parse(req.body);
+
+    const user = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (!user) {
+      return next(createError(404, "User not found"));
+    }
+
+    if (user.is_active === false) {
+      return next(createError(403, "User is not active"));
+    }
+
+    const updatedUser = await prisma.user.update({
+      where: { email },
+      data: {
+        ...(name && { name }),
+        ...(roles && { roles }),
+        ...(user_type && { user_type }),
+        ...(isActive !== undefined && { is_active: isActive }),
+        ...(departments && {
+          departments: {
+            deleteMany: {},
+            create: departments.map((departmentId) => ({
+              departmentId: Number(departmentId),
+            })),
+          },
+        }),
+      },
+      include: {
+        departments: {
+          include: {
+            department: true,
+          },
+        },
+      },
+    });
+
+    createSuccess(res, "User updated successfully", updatedUser);
+  } catch (error: any) {
+    return next(createError(500, error));
+  }
+};
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,14 @@
 import { Router } from "express";
 import authRoutes from "./auth.routes";
 import externalRoutes from "./external.routes";
-
+import userRoutes from "./user.routes";
+import departmentRoutes from "./department.routes";
 const router = Router();
 
 router.use("/auth", authRoutes);
 router.use("/external", externalRoutes);
+router.use("/user", userRoutes);
+router.use("/department", departmentRoutes);
 
 export default router;
+
